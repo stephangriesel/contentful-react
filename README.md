17f39e1ebea76c54c2064ee26df32df30292539c
Stephan Griesel <griesel.stephan@scotch-soda.com>
2022-02-25 13:21:12 +0100

    done

---
+++ README.md
@@ -0,0 +1,6 @@
+# Boiler for Contentful React Projects
+
+1. NPM install
+2. Create .env file in root
+3. Add Contentful Content Delivery API - access token in .env:  REACT_APP_CONTENTFUL_TOKEN=[yourtoken]
+4. Update content in Contentful
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import {useState, useEffect} from "react";
+
+const query = `
+{
+  pageCollection {
+    items {
+      title
+      logo {
+        url
+      }
+    }
+  }
+}
+`
+
+function App() {
+  // define the initial state
+  const [page, setPage] = useState(null);
+
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/wux9nfcl36wb/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authenticate the request
+          Authorization: `Bearer ${process.env.REACT_APP_CONTENTFUL_TOKEN}`,
+        },
+        // send the GraphQL query
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        // rerender the entire component with new data
+        setPage(data.pageCollection.items[0]);
+      });
+  }, []);
+
+  // show a loading screen case the data hasn't arrived yet
+  if (!page) {
+    return "Loading...";
+  }
+
+  // return statement and JSX template.
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={page.logo.url} className="App-logo" alt="logo" />
+        <p>{page.title}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
